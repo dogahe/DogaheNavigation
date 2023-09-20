f4e30198429f5864225a7bacca52dad2aabf6c8c
dogahe <dogahe@google.com>
2023-09-20 16:29:44 -0400

    save

---
+++ Package.swift
@@ -0,0 +1,30 @@
+import PackageDescription
+
+let package = Package(
+  name: "GoogleNavigation",
+  products: [
+    // Products define the executables and libraries a package produces, and make them visible to other packages.
+    .library(
+      name: "GoogleNavigation",
+      targets: ["GoogleNavigationTarget"]),
+  ],
+  dependencies: [
+    // Dependencies declare other packages that this package depends on.
+    // .package(url: /* package url */, from: "1.0.0"),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+    // Targets can depend on other targets in this package, and on products in packages this package depends on.
+    .binaryTarget(name: "GoogleNavigation", url: "https://github.com/dogahe/GoogleNavigation/releases/download/1.0.0/GoogleNavigation_3p.xcframework.zip", checksum: "724948ab2e1f78c9e71b102350da689d6dc600d9af400e0a5c4b31d6f961a4a5"),
+    .target(
+      name: "GoogleNavigationTarget",
+      dependencies: ["GoogleNavigation"],
+      path: "GoogleNavigation",
+      sources: ["dummy.m"],
+      resources: [
+        .copy("Resources/GoogleNavigation.bundle")
+      ],
+      publicHeadersPath: "Sources"
+    ),
+  ]
+)
