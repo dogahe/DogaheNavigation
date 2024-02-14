bdda1b1bca64cf6a84bc6267e95577b4dd269be8
Behzad Dogahe <dogahe@google.com>
2024-02-14 12:36:10 -0500

    save

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
   ],
   dependencies: [
     // Dependencies declare other packages that this package depends on.
-    .package(url: "https://github.com/dogahe/DogaheMaps", "1.0.15"..."1.0.15"),
+    .package(url: "https://github.com/dogahe/DogaheMaps", .exact("1.0.18")),
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
