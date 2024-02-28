747a1157c4b1f9a0b5235e560eca64a99f829f65
Behzad Dogahe <dogahe@google.com>
2024-02-28 16:40:06 -0500

    save

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
     // Targets can depend on other targets in this package, and on products in packages this package depends on.
-    .binaryTarget(name: "GoogleNavigation", url: "https://dl.google.com/geosdk/swiftpm/5.3.0/GoogleNavigation_3p.xcframework.zip", checksum: "67b7778bf459756febd7a8c2efa025caa0b7fd96e48d182f66543fc5d7a8bfcb"),
+    .binaryTarget(name: "GoogleNavigation", url: "https://dl.google.com/geosdk/swiftpm/5.4.0/GoogleNavigation_3p.xcframework.zip", checksum: "0c581d9738a7e0a61d63e38184b606013f105b843bb06a8d39adba4ae1331b42"),
     .target(
       name: "GoogleNavigationTarget",
       dependencies: [
@@ -27,11 +27,33 @@ let package = Package(
         .product(name: "GoogleMapsCore", package: "ios-maps-sdk"),
       ],
       path: "GoogleNavigation",
-      sources: ["dummy.m"],
+      sources: ["GMSEmpty.m"],
       resources: [
         .copy("Resources/GoogleNavigation.bundle")
       ],
-      publicHeadersPath: "Sources"
+      publicHeadersPath: "Sources",
+      linkerSettings: [
+        .linkedLibrary("xml2"),
+        .linkedFramework("Accelerate"),
+        .linkedFramework("AudioToolbox"),
+        .linkedFramework("AVFoundation"),
+        .linkedFramework("CoreData"),
+        .linkedFramework("CoreGraphics"),
+        .linkedFramework("CoreImage"),
+        .linkedFramework("CoreLocation"),
+        .linkedFramework("CoreTelephony"),
+        .linkedFramework("CoreText"),
+        .linkedFramework("GLKit"),
+        .linkedFramework("ImageIO"),
+        .linkedFramework("Metal"),
+        .linkedFramework("OpenGLES"),
+        .linkedFramework("QuartzCore"),
+        .linkedFramework("Security"),
+        .linkedFramework("SystemConfiguration"),
+        .linkedFramework("UIKit"),
+        .linkedFramework("UserNotifications"),
+        .linkedFramework("WebKit"),
+      ]
     ),
   ]
 )
