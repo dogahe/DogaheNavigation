83b73c3a7fa6567f7872cde00e0fda9776a803a4
Behzad Dogahe <dogahe@google.com>
2025-01-15 11:37:10 -0500

    Use latest map to be compatible

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
   ],
   dependencies: [
     // Dependencies declare other packages that this package depends on.
-    .package(url: "https://github.com/googlemaps/ios-maps-sdk", from: "9.2.0"),
+    .package(url: "https://github.com/dogahe/DogaheMaps", from: "1.0.29"),
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -30,7 +30,7 @@ let package = Package(
       name: "GoogleNavigationTarget",
       dependencies: [
         "GoogleNavigation",
-        .product(name: "GoogleMaps", package: "ios-maps-sdk"),
+        .product(name: "GoogleMaps", package: "DogaheMaps"),
       ],
       path: "GoogleNavigation",
       sources: ["GMSEmpty.m"],
